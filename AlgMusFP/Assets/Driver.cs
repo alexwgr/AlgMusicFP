a5529b5c8e06a452e1b42084a7ce24a2ec00b1b4
alexwgr <alexwgrinaldi@gmail.com>
2020-05-25 18:06:53 -0700

    yay

---
+++ AlgMusFP/Assets/Driver.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+
+public class Driver : MonoBehaviour
+{
+
+    public float MaxVelocity, BoostForce, Deceleration;
+
+    float directionAngle;
+
+
+    Transform auraTarget = null;
+    Vector2 auraEntryVelocity, auraPrevDirection;
+    float auraTimer = 0f;
+
+    bool boostLock = false;
+    public Rigidbody2D rigidBody;
+
+    public void pushVertical(float verticalDirection) {        
+        directionAngle = directionAngle + verticalDirection;
+    }
+
+    public void pushToDirection(Vector2 target) {
+        var displacement = Vector2.Angle(Vector2.right, target) - directionAngle;
+        directionAngle += Mathf.Sign(displacement) * Mathf.Min(50f, Mathf.Abs(displacement));
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Aura")) {
+            auraTarget = collision.gameObject.transform;
+            auraEntryVelocity = rigidBody.velocity;
+            auraPrevDirection = auraEntryVelocity.normalized;
+            boostLock = true;
+            auraTimer = 1f;
+
+
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Aura")) {
+            auraTarget = null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        directionAngle = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var direction = new Vector2(Mathf.Cos(directionAngle * Mathf.Deg2Rad), Mathf.Sin(directionAngle * Mathf.Deg2Rad));
+        var boostRequest = Input.GetButton("Jump");
+        if (boostRequest && !boostLock) {
+            rigidBody.AddForce(direction * BoostForce);
+        } else if (!boostRequest && boostLock) {
+            boostLock = false;
+        }
+
+        var verticalInput = Input.GetAxis("Vertical");
+
+
+        if (auraTarget != null && (boostLock || !boostRequest)) {
+
+
+            var displacement = (Vector2)(auraTarget.position - transform.position);
+            var displacementNorm = displacement.normalized;
+            var problemComponent = Vector2.Dot(rigidBody.velocity, displacementNorm);
+            var targetVelocity = (rigidBody.velocity - problemComponent * displacementNorm).normalized * MaxVelocity * 0.75f;
+
+            var idealRadius = auraTarget.GetComponent<CircleCollider2D>().radius * 0.75f;
+            var downComponent = displacementNorm * Mathf.Max(0, displacement.magnitude - idealRadius) / idealRadius;
+
+            targetVelocity += downComponent;
+
+            //var forceDirection = rigidBody.velocity -  * displacement;
+            rigidBody.velocity = auraTimer * auraEntryVelocity + (1 - auraTimer) * targetVelocity;
+            auraTimer = Mathf.Max(0, auraTimer - 0.1f);
+
+            directionAngle = Vector2.SignedAngle(Vector2.right, targetVelocity);
+
+            auraPrevDirection = rigidBody.velocity.normalized;
+            //pushToDirection(targetVelocity);
+            //rigidBody.AddForce(-0.5f * problemComponent * displacement);
+
+        } else {
+            if (!boostRequest || boostLock) {
+                rigidBody.velocity = rigidBody.velocity.normalized * (Mathf.Max(0, rigidBody.velocity.magnitude - Deceleration));
+            }
+        }
+
+
+        pushVertical(verticalInput * (boostRequest ? 2.5f : 6f));
+
+        
+
+        //direction = new Vector2(Mathf.Cos(rigidBody.rotation * Mathf.Deg2Rad), Mathf.Sin(rigidBody.rotation * Mathf.Deg2Rad));
+        rigidBody.SetRotation(directionAngle);
+
+        
+
+
+        if (rigidBody.velocity.magnitude > MaxVelocity) {
+            rigidBody.velocity = rigidBody.velocity.normalized * MaxVelocity;
+        }
+
+
+
+
+    }
+}
---
+++ AlgMusFP/Assets/Vec2Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Extensions {
+    public static class Vector2Extension {
+        public static Vector2 Rotate(this Vector2 v, float degrees) {
+            return Quaternion.Euler(0, 0, degrees) * v;
+        }
+
+        public static float Project(this Vector2 v, Vector2 target) {
+            //return v.magnitude * Mathf.Cos(Mathf.Deg2Rad * Vector2.Angle (v, target));
+            var projection = (v.x * target.x + v.y * target.y) / target.magnitude;
+            return projection;
+        }
+
+        public static Vector2 Midpoint(Vector2 v1, Vector2 v2) {
+            return new Vector2((v1.x + v2.x) / 2f, (v1.y + v2.y) / 2f);
+        }
+
+        public static Vector2 Flatten(this Vector3 v) {
+            return new Vector2(v.x, v.y);
+        }
+
+        public static Vector3 Explode(this Vector2 v) {
+            return new Vector3(v.x, v.y, 0);
+        }
+
+        public static float QuadrantAngle(this Vector2 v) {
+            var angle = Mathf.Atan(Mathf.Abs(v.y / v.x));
+
+            if (v.x >= 0 && v.y >= 0)
+                return angle;
+            else if (v.x < 0 && v.y >= 0)
+                return Mathf.PI - angle;
+            else if (v.x <= 0 && v.y < 0)
+                return Mathf.PI + angle;
+            else
+                return -angle;
+        }
+    }
+}
